35884f07539eea882a2ab74b9a7edb0401e2a487
Fotis <fzoump1993@gmail.com>
2025-09-16 18:55:18 +0300

    Domain Entities created.(Course,Professor,Student). Enum(Gender) created

---
+++ src/main/java/domain/Course.java
@@ -0,0 +1,11 @@
+package domain;
+
+
+import java.util.List;
+
+public class Course {
+
+  private long id;
+  private String description;
+  private int year;
+}
---
+++ src/main/java/domain/Student.java
@@ -0,0 +1,14 @@
+package domain;
+
+import java.time.LocalDate;
+
+public class Student {
+
+  private long id;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String phone;
+  private LocalDate birthday;
+  private Gender gender;
+}
