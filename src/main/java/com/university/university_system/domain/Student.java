be090111d8ba320a9763b23f0d0720d95dee704f
Fotis <fzoump1993@gmail.com>
2025-10-16 18:17:44 +0300

    Updated Student(updated annotations). Removed from StudentRepository (custom methods). Updated StudentService (removed custom methods, refactored method update, added deleteAllStudents method).

--- src/main/java/com/university/university_system/domain/Student.java
+++ src/main/java/com/university/university_system/domain/Student.java
@@ -4,12 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,23 +29,24 @@ import lombok.ToString;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "student")
 public class Student {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "id")
+  @Column(name = "id" , nullable = false , updatable = false)
   private long id;
 
-  @Column(name = "first_name")
+  @Column(name = "first_name" , nullable = false)
   private String firstName;
 
-  @Column(name = "last_name")
+  @Column(name = "last_name" , nullable = false)
   private String lastName;
 
-  @Column(name = "email")
+  @Column(name = "email" , unique = true)
   private String email;
 
-  @Column(name = "phone")
+  @Column(name = "phone" , unique = true)
   private String phone;
 
   @Column(name = "birthday")
@@ -53,7 +56,8 @@ public class Student {
   @Column(name = "gender")
   private Gender gender;
 
-  @ManyToMany
+  @ToString.Exclude
+  @ManyToMany(fetch = FetchType.EAGER)
   @JoinTable(name = "student_course",
       joinColumns = @JoinColumn(name = "student_id"),
       inverseJoinColumns = @JoinColumn(name = "course_id"))
@@ -61,7 +65,7 @@ public class Student {
 
   public void addCourse(Course course){
     if (course.getProfessor() == null) {
-      throw new IllegalStateException();
+      throw new IllegalStateException("No professor is teaching this course.");
     }
     if (courses == null){
       courses = new ArrayList<>();
---
+++ src/main/java/com/university/university_system/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.university.university_system.repository;
+
+import com.university.university_system.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
