41bec9e3098f89ab8cd0d08fa28df3c56c6f0124
Fotis <fzoump1993@gmail.com>
2025-10-16 18:18:29 +0300

    Updated StudentService (removed custom methods, refactored method update, added deleteAllStudents method).

--- src/main/java/com/university/university_system/service/StudentService.java
+++ src/main/java/com/university/university_system/service/StudentService.java
@@ -1,7 +1,7 @@
 package com.university.university_system.service;
 
 import com.university.university_system.domain.Student;
-import com.university.university_system.repo.StudentRepo;
+import com.university.university_system.repository.StudentRepository;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 
 public class StudentService {
   
-  private final StudentRepo studentRepo;
+  private final StudentRepository studentRepo;
 
   public Student create(Student student) {
     return studentRepo.save(student);
@@ -28,53 +28,35 @@ public class StudentService {
     return studentRepo.findAll();
   }
 
-  public Student findByFirstName(String name) {
-    return studentRepo.findByFirstName(name);
-  }
-
-  public Student findByLastName(String name) {
-    return studentRepo.findByLastName(name);
-  }
-
-  public Student findByEmail(String email) {
-    return studentRepo.findByEmail(email);
-  }
-
-  public Student findByPhone(String phone) {
-    return studentRepo.findByPhone(phone);
-  }
-
   public Student updateStudent(Student student) {
-    Optional<Student> existingStudentOpt = studentRepo.findById(student.getId());
+   studentRepo.findById(student.getId()).ifPresent(
+       existingStudent -> {
 
-    if (existingStudentOpt.isPresent()) {
-      Student existingStudent = existingStudentOpt.get();
-
-      if (student.getFirstName() != null) {
-        existingStudent.setFirstName(student.getFirstName());
-      }
-      if (student.getLastName() != null){
-        existingStudent.setLastName(student.getLastName());
-      }
-      if(student.getEmail()!= null){
-        existingStudent.setEmail(student.getEmail());
-      }
-      if(student.getBirthday()!=null){
-        existingStudent.setBirthday(student.getBirthday());
-      }
-      if(student.getGender()!=null){
-        existingStudent.setGender(student.getGender());
-      }
-      if(student.getPhone()!=null){
-        existingStudent.setPhone(student.getPhone());
-      }
-      if (student.getCourses() != null && !student.getCourses().isEmpty()) {
-        existingStudent.setCourses(student.getCourses());
-      }
-      return studentRepo.save(existingStudent);
-    }
-    return student;
-
+           if (student.getFirstName() != null) {
+             existingStudent.setFirstName(student.getFirstName());
+           }
+           if (student.getLastName() != null){
+             existingStudent.setLastName(student.getLastName());
+           }
+           if(student.getEmail() != null){
+             existingStudent.setEmail(student.getEmail());
+           }
+           if(student.getBirthday() !=null){
+             existingStudent.setBirthday(student.getBirthday());
+           }
+           if(student.getGender() !=null){
+             existingStudent.setGender(student.getGender());
+           }
+           if(student.getPhone() !=null){
+             existingStudent.setPhone(student.getPhone());
+           }
+           if (student.getCourses() != null) {
+             existingStudent.setCourses(student.getCourses());
+           }
+           studentRepo.save(existingStudent);
+         }
+         );
+   return student;
   }
 
   public void deleteById(Long id){
@@ -82,14 +64,8 @@ public class StudentService {
     studentRepo.delete(foundStudent);
   }
 
-  public void deleteByFirstName(String name){
-    studentRepo.deleteByFirstName(name);
+  public void deleteAllStudents(){
+    List<Student> students = studentRepo.findAll();
+    studentRepo.deleteAll(students);
   }
-  public void deleteByLastName(String name){
-    studentRepo.deleteByLastName(name);
-  }
-  public void deleteByEmail(String email){
-    studentRepo.deleteByEmail(email);
-  }
-  
 }
