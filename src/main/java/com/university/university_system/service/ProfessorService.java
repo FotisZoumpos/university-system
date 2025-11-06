4e46c9f733d675897822f22b2a3419e1c27aac4c
Fotis <fzoump1993@gmail.com>
2025-11-06 18:19:28 +0200

    Deleted findAllProfessors(). Refactored deleteAllProfessors()-> deleteAllByIds(). Split update method to two methods: update and updateProfessorCourse. Added @Transactional annotation.

--- src/main/java/com/university/university_system/service/ProfessorService.java
+++ src/main/java/com/university/university_system/service/ProfessorService.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class ProfessorService {
 
   private final ProfessorRepository professorRepo;
@@ -25,48 +24,52 @@ public class ProfessorService {
     return professorRepo.findById(id);
   }
 
-  public List<Professor> findAllProfessors() {
-    return professorRepo.findAll();
-  }
+  @Transactional
+  public Professor updateProfessorFields(Professor professor) {
+    professorRepo.findById(professor.getId()).ifPresent(existingProfessor -> {
 
-  public Professor updateProfessor(Professor professor) {
-    professorRepo.findById(professor.getId()).ifPresent(
-        existingProfessor -> {
-
-          if (professor.getFirstName() != null) {
-            existingProfessor.setFirstName(professor.getFirstName());
-          }
-          if (professor.getLastName() != null) {
-            existingProfessor.setLastName(professor.getLastName());
-          }
-          if (professor.getEmail() != null) {
-            existingProfessor.setEmail(professor.getEmail());
-          }
-          if (professor.getBirthday() != null) {
-            existingProfessor.setBirthday(professor.getBirthday());
-          }
-          if (professor.getGender() != null) {
-            existingProfessor.setGender(professor.getGender());
-          }
-          if (professor.getPhone() != null) {
-            existingProfessor.setPhone(professor.getPhone());
-          }
-          if (professor.getCourses() != null) {
-            existingProfessor.setCourses(professor.getCourses());
-          }
-          professorRepo.save(existingProfessor);
-        }
-    );
+      if (professor.getFirstName() != null) {
+        existingProfessor.setFirstName(professor.getFirstName());
+      }
+      if (professor.getLastName() != null) {
+        existingProfessor.setLastName(professor.getLastName());
+      }
+      if (professor.getEmail() != null) {
+        existingProfessor.setEmail(professor.getEmail());
+      }
+      if (professor.getBirthday() != null) {
+        existingProfessor.setBirthday(professor.getBirthday());
+      }
+      if (professor.getGender() != null) {
+        existingProfessor.setGender(professor.getGender());
+      }
+      if (professor.getPhone() != null) {
+        existingProfessor.setPhone(professor.getPhone());
+      }
+      professorRepo.save(existingProfessor);
+    });
     return professor;
   }
 
+  @Transactional
+  public Professor updateProfessorCourses(Professor professor) {
+    professorRepo.findById(professor.getId()).ifPresent(existingProfessor -> {
+      if (professor.getCourses() != null) {
+        existingProfessor.setCourses(professor.getCourses());
+      }
+      professorRepo.save(existingProfessor);
+    });
+    return professor;
+  }
+
+  @Transactional
   public void deleteById(Long id) {
     Professor foundProfessor = professorRepo.findById(id).orElseThrow();
-    if(foundProfessor.getCourses() != null){
-      for(Course course : foundProfessor.getCourses()){
+    if (foundProfessor.getCourses() != null) {
+      for (Course course : foundProfessor.getCourses()) {
         course.setProfessor(null);
-        if (course.getStudents() != null){
-          for (Student student : course.getStudents()){
+        if (course.getStudents() != null) {
+          for (Student student : course.getStudents()) {
             student.getCourses().remove(course);
           }
         }
@@ -75,12 +78,12 @@ public class ProfessorService {
     professorRepo.delete(foundProfessor);
   }
 
-  public void deleteAllProfessors() {
-    List<Professor> professors = professorRepo.findAll();
+  @Transactional
+  public void deleteAllById(List<Long> ids) {
+    List<Professor> professors = professorRepo.findAllById(ids);
     for (Professor professor : professors) {
       deleteById(professor.getId());
     }
-    professorRepo.deleteAll();
   }
 }
 
