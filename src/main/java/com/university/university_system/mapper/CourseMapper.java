4a3fa03b6e1ce640fe5dd3cbaca3678a7634a6ce
Fotis <fzoump1993@gmail.com>
2026-03-09 20:46:26 +0200

    Added @Mapping at CourseMapper and StudentMapper

--- src/main/java/com/university/university_system/mapper/CourseMapper.java
+++ src/main/java/com/university/university_system/mapper/CourseMapper.java
@@ -3,10 +3,11 @@ package com.university.university_system.mapper;
 import com.university.university_system.domain.Course;
 import com.university.university_system.dto.CourseDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CourseMapper {
-
+  @Mapping(target = "professor", ignore = true)
   CourseDto toDto(Course course);
 
   Course toEntity(CourseDto courseDto);
--- src/main/java/com/university/university_system/mapper/StudentMapper.java
+++ src/main/java/com/university/university_system/mapper/StudentMapper.java
@@ -3,10 +3,12 @@ package com.university.university_system.mapper;
 import com.university.university_system.domain.Student;
 import com.university.university_system.dto.StudentDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",uses = {CourseMapper.class})
 public interface StudentMapper {
 
+  @Mapping(target = "courses", ignore = false)
   StudentDto toDto(Student student);
 
   Student toEntity(StudentDto studentDto);
