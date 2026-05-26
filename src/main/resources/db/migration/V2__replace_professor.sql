d4e020b188d4b2d632ab8fc21b3729ddd6be3b7c
Fotis <fzoump1993@gmail.com>
2026-05-26 21:30:13 +0300

    feat(flyway): add new flyway script

---
+++ src/main/resources/db/migration/V2__replace_professor.sql
@@ -0,0 +1,5 @@
+DELETE FROM professor
+WHERE email = 'v@l.gr';
+
+INSERT INTO professor (birthday, email, first_name, gender, last_name, phone)
+VALUES ('1987-03-23','t@z','theo','MALE','zou','2103424020');
